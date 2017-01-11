27c160a746a99bc4685e27157ba855d2b5a8e401
[Stephen D. Van Hooser] <[vanhoosr@brandeis.edu]>
2017-01-10 19:48:46 -0500

    edited initialization file name, duties

---
+++ RunExperimentGlobals.m
@@ -0,0 +1,10 @@
+% RUNEXPERIMENTGLOBALS - Declare global variables for the RunExperiment app
+%
+%  VH_RunStimsLocally      0/1    Should we run stimulus scripts locally?
+%                                     (Usually for testing.)
+%  VH_UseParallel          0/1    Should we use parallel processing if we
+%                                    have it?
+
+global VH_RunStimsLocally
+
+global VH_UseParallel
