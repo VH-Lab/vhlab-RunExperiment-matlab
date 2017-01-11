b97d086ea89387cb241237d984130475272cb7f0
[Stephen D. Van Hooser] <[vanhoosr@brandeis.edu]>
2017-01-10 23:23:40 -0500

    RunExperimentInit modified

--- RunExperimentInit.m
+++ RunExperimentInit.m
@@ -1,11 +1,2 @@
-pwd = which('NelsonLabToolsInit');
-
-windowdiscriminator('default');
-cksmultipleunit([1 2],'','',[1.5],[]);
-wdcluster('default');
-multiextractor('default');
-dotdiscriminator('default');
-
 NelsonLabToolsInitLocal;
 
-clear pi pwd;
