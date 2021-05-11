e902b7fa13681f32ed29238c59d9577ecfcf367e
Stephen D. Van Hooser <vanhoosr@brandeis.edu>
2021-05-11 12:45:33 -0400

    updates to FerretTripleVarySheet defaults

--- uitools/Sheets/FerretTripleVarySheet.m
+++ uitools/Sheets/FerretTripleVarySheet.m
@@ -33,16 +33,17 @@ switch command
         p.angle=0;
         p.sFrequency=0.1;
         p.tFrequency=4;
-        p.nCycles=8; % 2 seconds
+        p.nCycles=2; % 2 seconds, with 3 loops
         p.rect=[0 0 800 600];
         p.contrast = 1;
-        p.distance = 25;
+        p.distance = 30;
         p.flickerType = 0; p.animType = 4; p.imageType = 2;
+	p.loops = 3;
         ps=periodicscript(p);
         
         gratingcontrolsheetlet_process(fig,'GratCtrl',usstruct.ds,'GratCtrlSetVars',ps,6,1.5,1,1);
-        triplegratingsheetlet_process(fig,'dirspatcont',usstruct.ds,'dirspatcontSetVars','','[0:30:360-30]','angle','[.01 .02 .04 .08 .16 .32]','sFrequency','[0.1:0.1:1]','contrast','GratCtrl',0,'ref');
-        triplegratingsheetlet_process(fig,'dirtempcont',usstruct.ds,'dirtempcontSetVars','','[0:30:360-30]','angle','[1 2 4 8 16]','tFrequency','[0.1:0.1:1]','contrast','GratCtrl',0,'ref');
+        triplegratingsheetlet_process(fig,'dirspatcont',usstruct.ds,'dirspatcontSetVars','','[0:45:180-45]','angle','[0.04 0.08 0.16 0.24 0.32 0.64 0.90 1.25]','sFrequency','[0.04 0.08 0.16 0.32 0.64 1]','contrast','GratCtrl',0,'ref');
+        triplegratingsheetlet_process(fig,'dirtempcont',usstruct.ds,'dirtempcontSetVars','','[0:45:180-45]','angle','[0.5 1 2 4 8 16 32]','tFrequency','[1]','contrast','GratCtrl',0,'ref');
     otherwise
         disp(['unknown command ' command])
 end
